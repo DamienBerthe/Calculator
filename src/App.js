3fd22f61790a8bf5f098affda1005cdffb220a79
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-06-30 03:02:37 +0200

    addition numéros

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [expression, setExpression] = useState([]);
+  const [total, setTotal] = useState(0);
+  const x= [];
+  for(let i=0; i<10; i++){
+    x.push(i)
+  }
+  const y = x.map(kek=><button onClick={()=> setExpression(expression=>[...expression, kek])}>{kek}</button>)
+
+  function calcul(x){
+    let y=0;
+    for(let i=0; i<x.length; i++){
+      if(Number.isInteger(x[i])===true){
+        y+=x[i]
+      }
+    }
+    return y;
+  }
+    
+
+  return(
+    <div>
+      <p>
+       {y}
+       <button onClick={()=> setExpression("")}>{"AC"}</button>
+       <button onClick={()=> setExpression(expression=>[...expression, '+'])}>{"+"}</button>
+       <button onClick={()=> setExpression(expression=>[...expression, '-'])}>{"-"}</button>
+       <button onClick={()=> setExpression(expression=>[...expression, '*'])}>{"*"}</button>
+       <button onClick={()=> setExpression(expression=>[...expression, '/'])}>{"/"}</button>
+       <button onClick={()=> setTotal(calcul(expression))}>{"="}</button>
+       <br />
+       expression : {expression}
+       <br />
+       total : {total}
+      </p>
     </div>
   );
 }
