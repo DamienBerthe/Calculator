d9f484d56936d55dd8bf2dc13c6afeea9b0a1cdf
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-07-18 17:38:02 +0200

    final js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 function App() {
   const [expression, setExpression] = useState('0');
-  //const [total, setTotal] = useState(0);
   const x = [];
   for (let i = 1; i < 10; i++) {
     x.push(i)
@@ -12,41 +11,35 @@ function App() {
 
   function filter(x, y) {
     if (/[+*/-]/.test(x.charAt(x.length - 1)) === true && /[+*/-]/.test(x.charAt(x.length - 2)) === true && /[+*/-]/.test(y) === true) {
-      if(x.charAt(x.length - 1) === y){
+      if (x.charAt(x.length - 1) === y) {
         return x
       }
-      else{
-        return x.slice(0, x.length - 2)+y
+      else {
+        return x.slice(0, x.length - 2) + y
       }
     }
     else {
-      if (x.charAt(x.length - 1) === y && /[+*/-]/.test(y) === true) {
+      if ((x.charAt(x.length - 1) === y && /[+*/-]/.test(y) === true) || (x === '0' && y === '0')) {
         return x
       }
-      else if (x === '0' && y ==='0'){
-        return x
-      }
-      else if (x=== '0' && /[1-9]/.test(y) === true){
+      else if (x === '0' && /[1-9]/.test(y) === true) {
         return y.toString()
       }
-      else if(/.*[+*/-]0/.test(x) === true && /[1-9]/.test(y) === true){
-        return x.slice(0, x.length - 1) + y
-      }
-      else if (((x.charAt(x.length - 1)) === '-' && /[+*/]/.test(y) === true) ||
-        (/[+*/]/.test(x.charAt(x.length - 1)) === true && /[+*/]/.test(y) === true && x.charAt(x.length - 1) !== y)) {
-        return x.slice(0, x.length - 1) + y
-      }
-      else if (/[+]/.test(x.charAt(x.length - 1)) === true && y === '-') {
+      else if ((/.*[+*/-]0/.test(x) === true && /[1-9]/.test(y) === true)
+      ||((x.charAt(x.length - 1)) === '-' && /[+*/]/.test(y) === true)
+      ||(/[+*/]/.test(x.charAt(x.length - 1)) === true && /[+*/]/.test(y) === true && x.charAt(x.length - 1) !== y)
+      ||(x.charAt(x.length - 1)==='+' && y === '-'))
+      {
         return x.slice(0, x.length - 1) + y
       }
       else return x + y
     }
   }
-  
-  function stringToExpression(obj){
+
+  function stringToExpression(obj) {
     // eslint-disable-next-line
     return Function('"use strict";return (' + obj + ')')();
-}
+  }
 
   return (
     <div>
@@ -54,32 +47,19 @@ function App() {
         {y}
         <button onClick={() => setExpression(filter(expression, '0'))}>{'0'}</button>
         <button onClick={() => setExpression("0")}>{"AC"}</button>
-        {//<button onClick={()=> setExpression(expression=>[...expression, '+'])}>{"+"}</button>
-        }
         <button onClick={() => setExpression(expression + '.')}>{"."}</button>
-        {/*<button onClick={() => setExpression(expression + '+')}>{"+"}</button>
-           <button onClick={() => setExpression(expression + '-')}>{"-"}</button>
-           <button onClick={() => setExpression(expression + '*')}>{"*"}</button>
-           <button onClick={() => setExpression(expression + '/')}>{"/"}</button>
-      */}
         <button onClick={() => setExpression(filter(expression, '+'))}>{"+"}</button>
         <button onClick={() => setExpression(filter(expression, '-'))}>{"-"}</button>
         <button onClick={() => setExpression(filter(expression, '*'))}>{"*"}</button>
         <button onClick={() => setExpression(filter(expression, '/'))}>{"/"}</button>
-
-        {//<button onClick={() => setTotal(stringToExpression(expression))}>{"="}</button>
-}
-        <button onClick={() => { if(/[^+*/-]/.test(expression.charAt(expression.length - 1)) === true) {
-          setExpression(stringToExpression(expression).toString())
-        }  
+        <button onClick={() => {
+          if (/[^+*/.-]/.test(expression.charAt(expression.length - 1)) === true) {
+            setExpression(stringToExpression(expression).toString())
+          }
         }
         }>{"="}</button>
         <br />
         {expression}
-       {/*
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: splitter(expression) }} />
-       */}
       </p>
     </div>
   );
