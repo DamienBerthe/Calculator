c6e098f871f9dfc7216651ac890e0c1a725c16b2
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-07-20 22:40:51 +0200

    100%

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Button from 'react-bootstrap/Button';
 
 function App() {
   const [expression, setExpression] = useState('0');
+  const [ans, setAns] = useState('0');
   const x = [];
   for (let i = 1; i < 10; i++) {
     x.push(i)
   }
-  const numbersNames =['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const y = x.map(kek => <Button variant="light" onClick={() => setExpression(filter(expression, kek))} id={numbersNames[kek-1]}>{kek}</Button>)
+  const numbersNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const y = x.map(kek => <Button className="button" variant="secondary" onClick={() => {setExpression(filter(expression, kek))}} id={numbersNames[kek - 1]}>{kek}</Button>)
 
   function filter(x, y) {
     if (/[+*/-]/.test(x.charAt(x.length - 1)) === true && /[+*/-]/.test(x.charAt(x.length - 2)) === true && /[+*/-]/.test(y) === true) {
@@ -24,14 +25,13 @@ function App() {
       if ((x.charAt(x.length - 1) === y && /[+*/.-]/.test(y) === true) || (x === '0' && y === '0')) {
         return x
       }
-      else if (x === '0' && /[1-9]/.test(y) === true) {
+      else if ((x === '0' && /[1-9]/.test(y) === true)||(ans!=='0'&& expression===ans && /[-9-9]/.test(y) === true)) {
         return y.toString()
       }
       else if ((/.*[+*/-]0/.test(x) === true && /[1-9]/.test(y) === true)
-      ||((x.charAt(x.length - 1)) === '-' && /[+*/]/.test(y) === true)
-      ||(/[+*/]/.test(x.charAt(x.length - 1)) === true && /[+*/]/.test(y) === true && x.charAt(x.length - 1) !== y)
-      ||(x.charAt(x.length - 1)==='+' && y === '-'))
-      {
+        || ((x.charAt(x.length - 1)) === '-' && /[+*/]/.test(y) === true)
+        || (/[+*/]/.test(x.charAt(x.length - 1)) === true && /[+*/]/.test(y) === true && x.charAt(x.length - 1) !== y)
+        || (x.charAt(x.length - 1) === '+' && y === '-')) {
         return x.slice(0, x.length - 1) + y
       }
       else return x + y
@@ -44,33 +44,39 @@ function App() {
   }
 
   return (
-    <div>
-      <p>
-        {y}
-        <Button variant="light" onClick={() => setExpression(filter(expression, '0'))} id ="zero">{'0'}</Button>
-        <Button variant="light" onClick={() => setExpression("0")} id ="clear">{"AC"}</Button>
-        <Button variant="light" onClick={() => {
-         if (/\d+\.\d+$/.test(expression) === false) {
-           setExpression(filter(expression, '.'))
-          }
-        }
-        } id="decimal">{"."}</Button>
-        <Button variant="light" onClick={() => setExpression(filter(expression, '+'))} id="add">{"+"}</Button>
-        <Button variant="light" onClick={() => setExpression(filter(expression, '-'))} id="subtract">{"-"}</Button>
-        <Button variant="light" onClick={() => setExpression(filter(expression, '*'))} id="multiply">{"*"}</Button>
-        <Button variant="light" onClick={() => setExpression(filter(expression, '/'))} id="divide">{"/"}</Button>
-        <Button variant="light" onClick={() => {
-          if (/[^+*/.-]/.test(expression.charAt(expression.length - 1)) === true) {
-            setExpression(stringToExpression(expression).toString())
-          }
-        }
-        } id="equals">{"="}</Button>
-        <br />
-        <div id="display">
+    <div id="salut">
+      <div id="display">
         {expression}
-        </div>
-        
-      </p>
+      </div>
+      {y}
+      <Button variant="secondary" className="button" onClick={() => setExpression(filter(expression, '0'))} id="zero">{'0'}</Button>
+      <Button variant="secondary" className="button" onClick={() => {
+        if (/\d+\.\d+$/.test(expression) === false) {
+          setExpression(filter(expression, '.'))
+        }
+      }
+      } id="decimal">{"."}</Button>
+      <Button variant="secondary" className="button" onClick={() => setExpression(filter(expression, '+'))} id="add">{"+"}</Button>
+      <Button variant="secondary" className="button" onClick={() => setExpression(filter(expression, '-'))} id="subtract">{"-"}</Button>
+      <Button variant="secondary" className="button" onClick={() => setExpression(filter(expression, '*'))} id="multiply">{"*"}</Button>
+      <Button variant="secondary" className="button" onClick={() => setExpression(filter(expression, '/'))} id="divide">{"/"}</Button>
+      <Button variant="success" className="button" onClick={() => setExpression("0")} id="clear">{"AC"}</Button>
+      <Button variant="success" className="button" onClick={() => {
+        if (/^\d$/.test(expression)=== false){
+          setExpression(expression.slice(0, expression.length - 1))
+        }
+        else {
+          setExpression('0')
+        }
+      }
+      }>{"CE"}</Button>
+      <Button variant="secondary" className="button" onClick={() => {
+        if (/[^+*/.-]/.test(expression.charAt(expression.length - 1)) === true) {
+          setExpression(stringToExpression(expression).toString());
+          setAns(stringToExpression(expression).toString());
+        }
+      }
+      } id="equals">{"="}</Button>
     </div>
   );
 }
